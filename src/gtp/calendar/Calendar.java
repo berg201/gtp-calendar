a7ab2bd0c7dcb3b414645b3a52517cdb4b54cf2b
berg <berg@LAPTOP-HT0G2L3S>
2020-01-05 16:56:15 +0900

    modified source

--- src/gtp/calendar/Calendar.java
+++ src/gtp/calendar/Calendar.java
@@ -6,42 +6,20 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		System.out.println("Hello, Calendar");
-		
-		//입력변수
-		int a = 0;
-		int b = 0;
-		int add1 = 0;
+		System.out.println(" 일   월   화   수   목   금   토" );
+		System.out.println("  1   2    3    4    5    6    7");
+		System.out.println("  8   9   10   11   12   13   14");
+		System.out.println(" 15  16   17   18   19   20   21");
+		System.out.println(" 22  23   24   25   26   27   28");
 		
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요.");
+		int month = scanner.nextInt();
 		
-		System.out.println("첫번째 값을 입력하세요. : ");
-		//첫번째 값
-		String s1 = scanner.next();
+		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
 		
-		System.out.println("두번째 값을 입력하세요. : ");
-		//두번째 값
-		String s2 = scanner.next();
-		
-		a = Integer.parseInt(s1);
-		b = Integer.parseInt(s2);
-		
-		add1 = a + b;
-		
-		
-//		addVal = f_inVal + n_inVal;
-		
-		System.out.println("Result : a = " + a + "\r" + 
-		                           " b = " + b + "\r" +
-				                   " a + b = " + add1 + " 입니다.");
-		
-		
-		
-		
-		
-		
-		
-
+		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
+		scanner.close();
 	}
 
 }
